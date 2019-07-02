e370d79816e1c4b516a9c62e00bf1e97ef23d8cc
Shen Sat <shensat18@gmail.com>
2019-07-02 20:28:03 +0100

    Include spaces in processor input to match user input

--- lib/processor.rb
+++ lib/processor.rb
@@ -8,8 +8,8 @@ class Processor
 	def process(line)
 		case line[0]
 		when 'I'
-			number_of_columns = line[1].to_i
-			number_of_rows = line[2].to_i
+			number_of_columns = line.split(/ /)[1].to_i
+			number_of_rows = line.split(/ /)[2].to_i
 			single_row = []
 			number_of_columns.times { single_row << 'O' }
 			number_of_rows.times { @table << Array.new(single_row) }
@@ -18,21 +18,21 @@ class Processor
 				row.collect! { |pixel| pixel = 'O' }
 			end
 		when 'L'
-			column_index = line[1].to_i - 1
-			row_index = line[2].to_i - 1
-			color = line[3]
+			column_index = line.split(/ /)[1].to_i - 1
+			row_index = line.split(/ /)[2].to_i - 1
+			color = line.split(/ /)[3]
 			@table[row_index][column_index] = color
 		when 'V'
-			column_index = line[1].to_i - 1
-			row_index_start = line[2].to_i - 1
-			row_index_finish = line[3].to_i - 1 
-			color = line[4]
+			column_index = line.split(/ /)[1].to_i - 1
+			row_index_start = line.split(/ /)[2].to_i - 1
+			row_index_finish = line.split(/ /)[3].to_i - 1 
+			color = line.split(/ /)[4]
 			(row_index_start..row_index_finish).each { |row_index| @table[row_index][column_index] = color }
 		when 'H'
-			column_index_start = line[1].to_i - 1
-			column_index_finish = line[2].to_i - 1
-			row_index = line[3].to_i - 1
-			color = line[4]
+			column_index_start = line.split(/ /)[1].to_i - 1
+			column_index_finish = line.split(/ /)[2].to_i - 1
+			row_index = line.split(/ /)[3].to_i - 1
+			color = line.split(/ /)[4]
 			(column_index_start..column_index_finish).each { |column_index| @table[row_index][column_index] = color }
 		end
 	end
--- spec/processor_spec.rb
+++ spec/processor_spec.rb
@@ -1,6 +1,5 @@
 require_relative '../lib/processor'
 #TODO Double digits eg a grid that is 250x250
-#TODO multiple lines of inputs
 #TODO lines are out-of-bounds
 #TODO what happens if I input is not first? If @table is empty, store commands, then run them at the end
 #TODO warning if 'C' command has inout after it
@@ -10,22 +9,22 @@ require_relative '../lib/processor'
 
 describe 'Processor' do
 
-	it 'should return an array within an array of one "O" for input "I11"' do
+	it 'should return an array within an array of one "O" for input "I 1 1"' do
 		processor = Processor.new
-		processor.process('I11')
+		processor.process('I 1 1')
 		expect(processor.table).to eq([['O']])		
 	end
 
-	it 'should return two arrays within an array, each containing two "O" for input "I22"' do
+	it 'should return two arrays within an array, each containing two "O" for input "I 2 2"' do
 		processor = Processor.new
-		processor.process('I22')
+		processor.process('I 2 2')
 		expect(processor.table).to eq([['O','O'], ['O','O']])		
 	end
 
 
-	it 'should return six arrays within an array, each containing five "O" for input "I56"' do
+	it 'should return six arrays within an array, each containing five "O" for input "I 5 6"' do
 		processor = Processor.new
-		processor.process('I56')
+		processor.process('I 5 6')
 		expect(processor.table).to eq([['O','O','O','O','O'], ['O','O','O','O','O'], ['O','O','O','O','O'], ['O','O','O','O','O'], ['O','O','O','O','O'], ['O','O','O','O','O']])		
 	end
 
@@ -43,62 +42,62 @@ describe 'Processor' do
 		expect(processor.table).to eq([['O', 'O'], ['O', 'O']])
 	end
 
-	it 'should convert the first element in the first array to "Z" for input L11Z' do
+	it 'should convert the first element in the first array to "Z" for input L 1 1 Z' do
 		processor = Processor.new
 		processor.table = [['A', 'B'], ['C', 'D']]
-		processor.process('L11Z')
+		processor.process('L 1 1 Z')
 		expect(processor.table).to eq([['Z', 'B'], ['C', 'D']])
 	end
 
-	it 'should convert the last element in the last array to "Z" for input L22Z' do
+	it 'should convert the last element in the last array to "Z" for input L 2 2 Z' do
 		processor = Processor.new
 		processor.table = [['A', 'B'], ['C', 'D']]
-		processor.process('L22Z')
+		processor.process('L 2 2 Z')
 		expect(processor.table).to eq([['A', 'B'], ['C', 'Z']])
 	end
 
 
-	it 'should convert the last element in the first array to "Z" for input L12Z' do
+	it 'should convert the last element in the first array to "Z" for input L 2 1 Z' do
 		processor = Processor.new
 		processor.table = [['A', 'B'], ['C', 'D']]
-		processor.process('L21Z')
+		processor.process('L 2 1 Z')
 		expect(processor.table).to eq([['A', 'Z'], ['C', 'D']])
 	end
 
-	it 'should convert the first column to "Z" for the input V113Z' do
+	it 'should convert the first column to "Z" for the input V 1 1 3 Z' do
 		processor = Processor.new
 		processor.table = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']]
-		processor.process('V113Z')
+		processor.process('V 1 1 3 Z')
 		expect(processor.table).to eq([['Z', 'B', 'C'], ['Z', 'E', 'F'], ['Z', 'H', 'I']])
 	end
 
-	it 'should convert first two-thirds of the last column to "Z" for the input V312Z' do
+	it 'should convert first two-thirds of the last column to "Z" for the input V 3 1 2 Z' do
 		processor = Processor.new
 		processor.table = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']]
-		processor.process('V312Z')
+		processor.process('V 3 1 2 Z')
 		expect(processor.table).to eq([['A', 'B', 'Z'], ['D', 'E', 'Z'], ['G', 'H', 'I']])
 	end
 
-	it 'should convert the first row to "Z" for the input H131' do
+	it 'should convert the first row to "Z" for the input H 1 3 1 Z' do
 		processor = Processor.new
 		processor.table = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']]
-		processor.process('H131Z')
+		processor.process('H 1 3 1 Z')
 		expect(processor.table).to eq([['Z', 'Z', 'Z'], ['D', 'E', 'F'], ['G', 'H', 'I']])
 	end
 
-	it 'should convert last two-thirds of the middle row to "Z" for the input H232Z' do
+	it 'should convert last two-thirds of the middle row to "Z" for the input H 2 3 2 Z' do
 		processor = Processor.new
 		processor.table = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']]
-		processor.process('H232Z')
+		processor.process('H 2 3 2 Z')
 		expect(processor.table).to eq([['A', 'B', 'C'], ['D', 'Z', 'Z'], ['G', 'H', 'I']])
 	end
 
 	it 'should handle multiple lines of input' do
 		processor = Processor.new
-		processor.process('I44')
-		processor.process('L23X')
-		processor.process('V424Y')
-		processor.process('H132Z')
+		processor.process('I 4 4')
+		processor.process('L 2 3 X')
+		processor.process('V 4 2 4 Y')
+		processor.process('H 1 3 2 Z')
 		expect(processor.table).to eq([['O','O','O','O'],['Z','Z','Z','Y'],['O','X','O','Y'],['O','O','O','Y']])
 	end
 
