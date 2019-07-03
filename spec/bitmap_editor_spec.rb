bab02d5fd0e340728e8e07325ec99cd33941bca4
Shen Sat <shensat18@gmail.com>
2019-07-03 15:42:00 +0100

    Add integration test for incorrect input

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -8,10 +8,18 @@ describe 'BitmapEditor' do
 		bitmap_editor.processor != nil
 	end
 
-	it 'should retrun test output for test input' do
+	it 'should return test output for test input' do
 		Dir.chdir(File.dirname(__FILE__))
 		file = File.open('../examples/test_input.txt')
 		bitmap_editor = BitmapEditor.new
 		expect { bitmap_editor.run(file) }.to output("OOOOO\nOOZZZ\nAWOOO\nOWOOO\nOWOOO\nOWOOO\n").to_stdout
 	end
+
+	it 'should return "Incorrect input" error for incorrect input' do
+		Dir.chdir(File.dirname(__FILE__))
+		file = File.open('../examples/incorrect_input.txt')
+		bitmap_editor = BitmapEditor.new
+		expect{ bitmap_editor.run(file) }.to raise_error('Incorrect input')
+	end
+
 end
